5152846358edbc985476bdc9603f4a89cd1776f0
CarolPatricio <carolpatricio28@hotmail.com>
2022-11-06 15:40:33 -0300

    feat:create serializer and finish clinic views

--- PetClinicAPI/apps/clinic/serializers.py
+++ PetClinicAPI/apps/clinic/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from PetClinicAPI.apps.client.models import Veterinary, Drug, Appointment
+from PetClinicAPI.apps.clinic.models import Veterinary, Drug, Appointment
+from PetClinicAPI.apps.client.serializers import PetSerializer
 
 class VeterinarySerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
@@ -25,3 +26,16 @@ class DrugSerializer(serializers.ModelSerializer):
 class AppointmentSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
     date = serializers.DateField()
+    description = serializers.CharField(max_length=255)
+    diagnosis = serializers.CharField(max_length=255)
+    drugs = DrugSerializer(many=True, read_only=True)
+    drugs_id = serializers.UUIDField(many=True, write_only=True)
+    pet = PetSerializer(read_only=True)
+    pet_id = serializers.UUIDField(write_only=True)
+    veterinary = VeterinarySerializer(read_only=True)
+    veterinary_id = serializers.UUIDField(write_only=True)
+
+    class Meta:
+        model = Appointment
+        fields = '__all__'
+        ref_name = None
--- PetClinicAPI/apps/clinic/views.py
+++ PetClinicAPI/apps/clinic/views.py
@@ -1,12 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 
 from PetClinicAPI.apps.clinic.models import Veterinary, Drug, Appointment
+from PetClinicAPI.apps.clinic.serializers import VeterinarySerializer, DrugSerializer, AppointmentSerializer
 
 class VeterinaryViewSet(ModelViewSet):
     queryset = Veterinary.objects.all()
+    serializer_class = VeterinarySerializer
 
 class DrugViewSet(ModelViewSet):
     queryset = Drug.objects.all()
+    serializer_class = DrugSerializer
 
 class AppointmentViewSet(ModelViewSet):
     queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
